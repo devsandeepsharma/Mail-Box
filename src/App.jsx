2d9bb3f3c85fd61267091de45270d39a84627750
Sandeep <sandipsharma2445@gmail.com>
2025-05-05 16:24:33 +0530

    react router dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+
 const App = () => {
-  return (
-    <div style={{
-        display:"flex", 
-        flexDirection: "column", 
-        justifyContent: "center", 
-        alignItems: "center",
-        minHeight: "100vh"
-      }}
-    >
-      <h1>Mail Box</h1>
-    </div>
-  )
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <h1>All Mails Page</h1>
+        },
+        {
+          path: "/landing",
+          element: <h1>Landing Page</h1>
+        },
+        {
+          path: "/profile",
+          element: <h1>Profile Page</h1>
+        },
+        {
+          path: "/sent",
+          element: <h1>Sent Mails Page</h1>
+        },
+        {
+          path: "/login",
+          element: <h1>Login Page</h1>
+        },
+        {
+          path: "/signup",
+          element: <h1>Signup Page</h1>
+        },
+      ]
+    }
+  ])
+  
+  return <RouterProvider router={router} />
 }
 
 export default App;
