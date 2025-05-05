08f355c451a754d782ccb5e2831187e236a56930
Sandeep <sandipsharma2445@gmail.com>
2025-05-05 16:11:00 +0530

    project started

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div style={{
+        display:"flex", 
+        flexDirection: "column", 
+        justifyContent: "center", 
+        alignItems: "center",
+        minHeight: "100vh"
+      }}
+    >
+      <h1>Mail Box</h1>
+    </div>
+  )
+}
+
+export default App;
