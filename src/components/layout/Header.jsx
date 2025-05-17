6334588ce69e45c25474b76b8fe7f40c6aaf999a
Sandeep <sandipsharma2445@gmail.com>
2025-05-17 14:02:59 +0530

    header updated

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./header.css";
 import Button from "../ui/Button";
 
 const Header = () => {
+
+    const { authenticate } = useSelector(state => state.auth);
 
     const [toggleNav, setToggleNav] = useState(false);
 
@@ -15,7 +18,7 @@ const Header = () => {
     return (
         <header className="header">
             <a className="skip-to-main-content" href="#main">Skip to main content</a>
-            <Link className="header__logo" to="/" >Mail Box</Link>
+            <Link className="header__logo" to={authenticate ? "/": "/landing"} >Mail Box</Link>
             <nav className="header__nav" aria-label="primary navigation">
                 <Button 
                     className="nav__button" 
@@ -29,12 +32,30 @@ const Header = () => {
                     <div className="line three"></div>
                 </Button>
                 <ul id="navlist" className="nav__list">
-                    <li className="nav__item link-btn">
-                        <Button to="/login" variant="outline" isLink onClick={toggle}>Login</Button>
-                    </li>
-                    <li className="nav__item link-btn">
-                        <Button to="/signup" isLink onClick={toggle}>Sign Up</Button>
-                    </li>
+                    {
+                        authenticate ? (
+                            <>
+                                <li className="nav__item">
+                                    <Link to="/sent" onClick={toggle}>Sent Mail</Link>
+                                </li>
+                                <li className="nav__item">
+                                    <Link to="/profile" onClick={toggle}>My Profile</Link>
+                                </li>
+                                <li className="nav__item link-btn">
+                                    <Button to="/compose" onClick={toggle} isLink>Compose Email</Button>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="nav__item link-btn auth">
+                                    <Button to="/login" variant="outline" isLink onClick={toggle}>Login</Button>
+                                </li>
+                                <li className="nav__item link-btn auth">
+                                    <Button to="/signup" isLink onClick={toggle}>Sign Up</Button>
+                                </li>
+                            </>
+                        )
+                    }
                 </ul>
             </nav>
         </header>
